a771fba683d897501f5d57c94cdec4f5051a194e
Alex Tsankov <antsankov@gmail.com>
2016-12-13 18:14:08 -0700

    refactor parsing

--- digestor/digestor.py
+++ digestor/digestor.py
@@ -1,5 +1,7 @@
 import xmltodict
 import argparse
+from datetime import datetime
+
 
 OBSERVED_ROUTE_IDS = set([
              '11',  # Vail-Vail Pass
@@ -13,43 +15,80 @@ OBSERVED_ROUTE_IDS = set([
              ])
 
 VAIL_ROUTES = set([11,10,9,6,5060,4,3,2])
-COPPER_ROUTES = set([9,6,5060,4,3,2])
-BRECK_ROUTES = set([9,6,5060,4,3,2])
-KEYSTONE_ROUTES = set([6,5060,4,3,2])
-ABASIN_ROUTES = set([6,5060,4,3,2])
+COPPER_BRECK_ROUTES = set([9,6,5060,4,3,2])
+KEYSTONE_ABASIN_ROUTES = set([6,5060,4,3,2])
 
 
 class Resort:
 
-    def __init__(self, name):
+    def __init__(self, name, watched_routes, raw_routes):
         self.name = name
+        self.watched_routes = watched_routes
+        self.raw_routes = raw_routes
+        
         self.closed_routes = []
         self.hazardous_routes = []
-        self.dates = set([])
+        self.dates = []
         # TODO is the date of everything always the same? 
 
-    def generate_message(self):
+        for route in self.raw_routes:
+            if int(route['rc:WeatherRouteId']) in self.watched_routes:
+                route_state = {
+                        'id' : int(route['rc:WeatherRouteId']),
+                        'route_name' : route['rc:RouteName'],
+                        'date' : datetime.strptime(route['rc:CalculatedDate'].split('.')[0], '%Y-%m-%dT%H:%M:%S'),
+                        'condition' : route['rc:RoadConditionCategoryTxt'],
+                        'condition_code' : float(route['rc:RoadConditionCategoryCd']),
+                        'hazardous' : bool(route['rc:IsHazardousCondition']),
+                        }
+ 
+                if route_state['condition'] == 'Closed' or route_state['condition_code'] == 1:
+                    self.closed_routes.append(route_state['route_name'])
+                    self.dates.append(route_state['date'])
+                
+                elif route_state['hazardous']:   
+                    self.hazardous_routes.append(route_state['route_name'])
+                    self.dates.append(route_state['date'])
+
+    def generate_message(self): 
         if len(self.closed_routes) != 0:
-            return 'I70 is CLOSED between {route_names}. This affects {resort}. Calculated on {date}.'.format(
-                    route_names=', '.join(self.closed_routes), resort=self.name, date=self.dates) 
+            return 'I70 is CLOSED between {aggregate_route}. This affects {resort}. Calculated on {date}.'.format(
+                    aggregate_route= route_digest(self.closed_routes), resort=self.name, date=earliest_date(self.dates)) 
         
         elif len(self.hazardous_routes) != 0:
-            return 'I70 is open, but marked hazardous between {route_names}. This affects {resort}. Calculated on {date}.'.format(
-                     route_names=', '.join(self.hazardous_routes), resort=self.name, date=self.dates) 
+            return 'I70 is OPEN, but is being impacted by weather between {aggregate_route}. This affects {resort}. Calculated on {date}.'.format(
+                     aggregate_route= route_digest(self.hazardous_routes), resort=self.name, date=earliest_date(self.dates)) 
 
         else:  
-            return 'I70 is open, and makred as safe. This affects {resort}. Calculated on {date}.'.format(
-                    resort=self.name, date=self.dates)
+            return 'I70 is OPEN, and unaffected by weather. This affects {resort}. Calculated on {date}.'.format(
+                    resort=self.name, date=earliest_date(dates))
 
+def route_digest(route_names):
+    first = route_names[0].split('-')[0]
+    last = route_names[-1].split('-')[1]
+    return '{0} and {1}'.format(first, last)
+
+def earliest_date(dates):
+    earliest = dates[0]
+    for date in dates:
+        if date < earliest:
+            earliest = date
+
+    return earliest.strftime('%m/%d @ %I:%M %p')
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--live', type=bool, default=False)
     args = parser.parse_args() 
-    raw = gather_observed_routes(args.live) 
-    vail = parse_routes(raw)
+    raw = gather_observed_routes(args.live)
+
+    vail = Resort('Vail', VAIL_ROUTES, raw) 
+    breck = Resort('Breckenridge', COPPER_BRECK_ROUTES, raw) 
+    arapahoe_basin = Resort('Araphaoe Basin', KEYSTONE_ABASIN_ROUTES, raw) 
 
     print(vail.generate_message())
+    print(breck.generate_message())
+    print(arapahoe_basin.generate_message())
 
 def gather_observed_routes(live=False):
     """
@@ -80,38 +119,6 @@ def gather_observed_routes(live=False):
             observed_routes.append(route)
  
     return observed_routes
-
-
-def parse_routes(raw_routes):
-    """
-    Converts observed routes into sane python route dictionary. 
-    """
-
-    vail = Resort('Vail') 
-
-    closed_routes = []
-    hazardous_routes = [] 
-    for route in raw_routes: 
-        route_state = {
-                'id' : int(route['rc:WeatherRouteId']),
-                'route_name' : route['rc:RouteName'],
-                'date' : route['rc:CalculatedDate'], 
-                'condition' : route['rc:RoadConditionCategoryTxt'],
-                'condition_code' : float(route['rc:RoadConditionCategoryCd']),
-                'hazardous' : bool(route['rc:IsHazardousCondition']),
-                }
- 
-        if route_state['condition'] == 'Closed' or route_state['condition_code'] == 1:
-            if route_state['id'] in VAIL_ROUTES:
-                vail.closed_routes.append(route_state['route_name'])
-                vail.dates.add(route_state['date'])
-        
-        elif route_state['hazardous']:  
-            if route_state['id'] in VAIL_ROUTES:
-                vail.hazardous_routes.append(route_state['route_name'])
-                vail.dates.add(route_state['date'])
-                      
-    return vail
 
 
 if __name__ == '__main__':
