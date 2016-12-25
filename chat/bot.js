9645973411a7295ede7fdc29a7f9baccaa4f68a1
Alex Tsankov <antsankov@gmail.com>
2016-12-24 23:49:17 -0500

    working redis promises

--- chat/bot.js
+++ chat/bot.js
@@ -6,29 +6,26 @@ const redisOptions = {
   port: "6379"
 };
 
-var redis = require('redis'),
-  client = redis.createClient(redisOptions);
+var bluebird = require('bluebird');
+var redis = require('redis');
+bluebird.promisifyAll(redis.RedisClient.prototype); 
+client = redis.createClient(redisOptions);
 
 const resorts = ['Arapahoe Basin', 'Breck', 'Keystone', 'Vail', 'Copper Mountain'];
 
 module.exports = botBuilder(request => {
   console.log("Req test: " + request.text);
   // if it's in a resort
-  if (resorts.indexOf(request.text) >= 0){
-    console.log('Hit it');
-    var vail = client.get('hash', (err, value) => { 
-      return value;
-    });
-    console.log(typeof(vail));
-    console.log(vail);
-    console.log(vail.toString());
-    message = new fbTemplate.Text(vail);
-    return message.get();
+  if (resorts.indexOf(request.text) >= 0){ 
+    return client.getAsync(request.text).then(res => {
+      console.log(res);
+      return res;
+    }); 
   } else {
     message = new fbTemplate.Text('Which resort do you want to know about?');
     resorts.forEach(function(resort){
       message.addQuickReply(resort, resort);
     });
-    return message.get();    
+    return message.get(); 
   } 
 });
