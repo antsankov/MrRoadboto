d08447773017a35722969e39ba13aeaa1ec6e01f
Alex Tsankov <antsankov@gmail.com>
2017-01-17 19:25:34 -0700

    added in getting started

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # MrRoaboto
-This repo is a FB messenger bot service that serves Colorado's Department of Transportation (CDOT) XML feeds for rode closure information and alerts. All of the subdirectories are AWS Lambda functions handling different parts of the bots functionality.
+This repo is a FB messenger bot service that serves Colorado's Department of Transportation (CDOT) XML feeds for rode closure information and alerts. All of the subdirectories are AWS Lambda functions handling different parts of the bot's functionality.
 
 # Where does MrRoadboto live?
 MrRoadboto's page is [here](https://www.facebook.com/roadboto/). These messages go directly to the chat lambda instance for parsing.
 
 # Lambda Functions
 
-## /chat
+## chat
 This lambda handles user interaction between FB and the backend. This function only contains messaging logic. 
+
+## digestor
+Pulls from CDOT, parses weather information, and put's it in the DB.
 
--- chat/bot.js
+++ chat/bot.js
@@ -11,6 +11,8 @@ var redis = require('redis');
 bluebird.promisifyAll(redis.RedisClient.prototype); 
 client = redis.createClient(redisOptions);
 
+const welcome = 'Hello. I can tell you if I70 is open between the major Colorado ski resorts and the Front Range. Select a resort from one of the buttons below.';
+
 const resorts = [
   'Vail',
   'Copper Mountain', 'Breckenridge', 
@@ -29,6 +31,10 @@ function MakeFB(content){
 
 module.exports = botBuilder(request => {
   console.log('req: ' + request.text);
+  
+  if (request.text === 'Get Started'){
+    return MakeFB(welcome);
+  }
   // if it's in a resort
   if (resorts.indexOf(request.text) >= 0){ 
     return client.getAsync(request.text).then((res,err) => {
@@ -39,6 +45,6 @@ module.exports = botBuilder(request => {
       return MakeFB(res);
     });
   } else {
-    return MakeFB('Which resort do you need road info about?');
+    return MakeFB('Which resort do you need I70 info about?');
   } 
 });
